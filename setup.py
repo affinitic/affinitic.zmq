fa1590809ad741558d4785ce3689063abb9e14e5
Sébastien Sirtoli <sebastien.sirtoli@affinitic.be>
2016-01-20 17:14:51 +0100

    Update long_description link ##

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 version = '0.1'
 
-long_description = (open('README.md').read())
+long_description = (open('README.rst').read())
 
 setup(name='affinitic.zmq',
       version=version,
